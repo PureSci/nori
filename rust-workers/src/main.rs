ceba3658e91b142aef6d0a844e3c5a7245760b21
Halit Yusuf Bilgen <halityusufbilgen@gmail.com>
2023-05-10 19:08:54 +0200

    implemented find_series to rust

--- rust-workers/src/main.rs
+++ rust-workers/src/main.rs
@@ -3,12 +3,14 @@ use card_handler::{card_handler_loop, CardsHandleType, Character};
 use drop::drop_ocr_loop;
 use image::{DynamicImage, ImageFormat};
 use node_bridge::NodeBridge;
+use series_handler::{series_handler_loop, Series, SeriesHandleType};
 use tokio::sync::mpsc;
 mod drop;
 use tokio::sync::mpsc::Sender;
 use tokio::sync::oneshot;
 mod captcha;
 mod card_handler;
+mod series_handler;
 
 #[tokio::main]
 async fn main() {
@@ -16,11 +18,15 @@ async fn main() {
     let (drop_sender, drop_receiver) = mpsc::channel(1);
     let (captcha_sender, captcha_receiver) = mpsc::channel(1);
     let (card_sender, card_receiver) = mpsc::channel(1);
+    let (series_sender, series_receiver) = mpsc::channel(1);
     bridge.register_async("ocr_drop", ocr_drop, Some(drop_sender));
     bridge.register_async("find_cards", find_cards, Some(card_sender.clone()));
+    bridge.register_async("find_series", find_series, Some(series_sender.clone()));
     bridge.register_async("ocr_captcha", ocr_captcha, Some(captcha_sender));
     tokio::spawn(card_handler_loop(card_receiver, bridge.clone()));
+    tokio::spawn(series_handler_loop(series_receiver, bridge.clone()));
     tokio::spawn(update_card_loop(card_sender.clone(), bridge.clone()));
+    tokio::spawn(update_series_loop(series_sender.clone(), bridge.clone()));
     tokio::spawn(drop_ocr_loop(
         drop_receiver,
         bridge.clone(),
@@ -81,12 +87,36 @@ async fn update_card_loop(sender: Sender<CardsHandleType>, bridge: NodeBridge) {
     bridge.close().await;
 }
 
+#[allow(unreachable_code)]
+async fn update_series_loop(sender: Sender<SeriesHandleType>, bridge: NodeBridge) {
+    loop {
+        let card: Series =
+            serde_json::from_str(bridge.receive("update_series").await.unwrap().as_str()).unwrap();
+        sender
+            .send(SeriesHandleType::UpdateSeries(card))
+            .await
+            .unwrap();
+    }
+    bridge.close().await;
+}
+
 async fn find_cards(params: Vec<String>, sender: Option<Sender<CardsHandleType>>) -> String {
     let characters: Vec<Character> = serde_json::from_str(params[0].as_str()).unwrap();
     let (return_sender, return_receiver) = oneshot::channel();
     sender
         .unwrap()
         .send(CardsHandleType::FindCard(characters, return_sender))
+        .await
+        .unwrap();
+    return_receiver.await.unwrap()
+}
+
+async fn find_series(params: Vec<String>, sender: Option<Sender<SeriesHandleType>>) -> String {
+    let series: Vec<Series> = serde_json::from_str(params[0].as_str()).unwrap();
+    let (return_sender, return_receiver) = oneshot::channel();
+    sender
+        .unwrap()
+        .send(SeriesHandleType::FindSeries(series, return_sender))
         .await
         .unwrap();
     return_receiver.await.unwrap()
---
+++ rust-workers/src/series_handler.rs
@@ -0,0 +1,141 @@
+use mongodb::{
+    bson::{doc, Document},
+    options::{ClientOptions, UpdateOptions},
+    Client, Database,
+};
+use node_bridge::NodeBridge;
+use rayon::prelude::*;
+use tokio::sync::{mpsc::Receiver, oneshot};
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct Series {
+    pub name: String,
+    pub wl: Option<u32>,
+}
+
+#[derive(Debug)]
+pub enum SeriesHandleType {
+    FindSeries(Vec<Series>, oneshot::Sender<String>),
+    UpdateSeries(Series),
+}
+
+#[allow(unreachable_code)]
+pub async fn series_handler_loop(mut receiver: Receiver<SeriesHandleType>, bridge: NodeBridge) {
+    let db_url = std::env::var("MONGODB_URL").unwrap();
+    let db = Client::with_options(ClientOptions::parse(db_url.clone()).await.unwrap())
+        .unwrap()
+        .database(
+            db_url
+                .split('/')
+                .last()
+                .unwrap_or_default()
+                .split('?')
+                .next()
+                .unwrap_or_default(),
+        );
+    let collection = db.collection::<Series>("analysis_series");
+    let mut cursor = collection.find(None, None).await.unwrap();
+    let mut series = vec![];
+    while cursor.advance().await.unwrap() {
+        series.push(cursor.deserialize_current().unwrap());
+    }
+    bridge.send("init", true).unwrap();
+    loop {
+        //let (cards, return_sender) = receiver.recv().await.unwrap();
+        match receiver.recv().await.unwrap() {
+            SeriesHandleType::FindSeries(cards, return_sender) => {
+                let found = (0..3)
+                    .into_par_iter()
+                    .map(|i| {
+                        let card = &cards[i].name;
+                        //card.retain(|c| !c.is_whitespace());
+                        let is_dot_card = card.ends_with("...");
+                        match series.par_iter().find_any(|serie| {
+                            let is_dot_serie = serie.name.ends_with("...");
+                            if is_dot_serie && is_dot_card {
+                                if serie.name.len() > card.len() {
+                                    return serie.name.starts_with(card);
+                                } else {
+                                    return card.starts_with(&serie.name);
+                                }
+                            } else if is_dot_serie && !is_dot_card {
+                                return card.starts_with(&serie.name);
+                            } else if (!is_dot_serie) && is_dot_card {
+                                return serie.name.starts_with(card);
+                            } else {
+                                return &serie.name == card;
+                            }
+                        }) {
+                            None => cards[i].clone(),
+                            Some(found) => found.to_owned(),
+                        }
+                    })
+                    .collect::<Vec<Series>>();
+                let mut return_str = "[".to_string();
+                for item in found.iter().take(3) {
+                    let wl = match &item.wl {
+                        Some(wishlist) => wishlist.to_string(),
+                        None => "?".to_string(),
+                    };
+                    return_str
+                        .push_str(&format!(r#"{{"name":"{}", "wl": "{}"}},"#, item.name, wl,));
+                }
+                return_str.pop();
+                return_str.push(']');
+                return_sender.send(return_str).unwrap();
+            }
+            SeriesHandleType::UpdateSeries(cardo) => {
+                let card = &cardo.name;
+                let is_dot_card = card.ends_with("...");
+                match series.par_iter_mut().find_any(|serie| {
+                    let is_dot_serie = serie.name.ends_with("...");
+                    if is_dot_serie && is_dot_card {
+                        if serie.name.len() > card.len() {
+                            return serie.name.starts_with(card);
+                        } else {
+                            return card.starts_with(&serie.name);
+                        }
+                    } else if is_dot_serie && !is_dot_card {
+                        return card.starts_with(&serie.name);
+                    } else if (!is_dot_serie) && is_dot_card {
+                        return serie.name.starts_with(card);
+                    } else {
+                        return &serie.name == card;
+                    }
+                }) {
+                    None => {
+                        if !is_dot_card {
+                            series.push(cardo.clone());
+                            tokio::spawn(update_series(db.clone(), cardo.clone()));
+                        }
+                    }
+                    Some(found_series) => {
+                        found_series.wl = cardo.wl;
+                        tokio::spawn(update_series(db.clone(), found_series.clone()));
+                    }
+                };
+            }
+        }
+    }
+    bridge.close().await;
+}
+
+async fn update_series(database: Database, card: Series) {
+    database
+        .collection::<Document>("analysis_series")
+        .update_one(
+            doc! {
+                "name": &card.name
+            },
+            doc! {
+                "$set": {
+                    "wl": &card.wl.as_ref().unwrap()
+            }
+            },
+            UpdateOptions::builder().upsert(true).build(),
+        )
+        .await
+        .unwrap();
+}
