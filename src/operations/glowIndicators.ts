afdeb0b5c719db432c700c95e8cf057492530384
Halit Yusuf Bilgen <halityusufbilgen@gmail.com>
2023-06-11 21:45:20 +0200

    add svg visualizer

--- src/operations/glowIndicators.ts
+++ src/operations/glowIndicators.ts
@@ -3,11 +3,33 @@ import { getServerConfig, getUserConfig } from "../utils/databaseHandler.js";
 import fetch from "node-fetch";
 import { createCanvas, loadImage } from "canvas";
 import Constants from "../utils/Constants.js";
+import { client } from "../index.js";
+
+interface GlowAPIResponse {
+    glows: APIGlowData[]
+}
+
+interface APIGlowData {
+    _id: string,
+    empty: boolean,
+    code2: string | null,
+    super: boolean,
+    viewcode: string,
+    name: string,
+    code: string
+}
+
+let editWaiting: {
+    [key: string]: {
+        glows: APIGlowData[],
+        msg: Message
+    }
+} = {};
 
 const glowIndicatorEmoji = parseEmoji(Constants.GLOWINFO_EMOJI)!;
 
 export function filter(message: Message): boolean {
-    return message.embeds?.[0]?.title == "SOFI: GLOW" ?? false;
+    return (message.embeds?.[0]?.title == "SOFI: GLOW" || message.embeds?.[0]?.title == "GLOW PALETTE:") ?? false;
 }
 
 export async function run(message: Message) {
@@ -58,7 +80,69 @@ export async function handleGlowIndicator(message: Message | PartialMessage, use
         const color = message.embeds[0].hexColor;
         if (color == "#ec91c8" || !color) return;
         message.reply(normalGlow(color, message.embeds[0].color!));
+    } else if (message.embeds?.[0]?.title == "GLOW PALETTE:") {
+        const userId = message.embeds?.[0]?.url?.split("/").pop()!;
+        const glows = message.embeds[0].description?.split("\n").map(rawGlow => rawGlow.split(" • `#")[1].split("`")[0])!;
+        const [attachment, apiGlows] = await glowImage(userId, glows);
+        message.reply({
+            files: [attachment]
+        }).then(msg => {
+            editWaiting[message.id] = {
+                glows: apiGlows, msg
+            };
+            setTimeout(() => {
+                delete editWaiting[message.id];
+            }, 60_000);
+        });
     }
+}
+
+client.on("messageUpdate", async (_, message) => {
+    const editWaitingData = editWaiting[message.id];
+    if (editWaitingData) {
+        const userId = message.embeds?.[0]?.url?.split("/").pop()!;
+        const glows = message.embeds[0].description?.split("\n").map(rawGlow => rawGlow.split(" • `#")[1].split("`")[0])!;
+        const [attachment, _] = await glowImage(userId, glows, editWaitingData.glows);
+        editWaitingData.msg.edit({
+            files: [attachment]
+        });
+    }
+});
+
+async function glowImage(owner: string, viewCodes: string[], glows?: APIGlowData[]): Promise<[AttachmentBuilder, APIGlowData[]]> {
+    if (!glows) glows = ((await (await fetch(`https://api.sofi.gg/glows/${owner}`, {
+        "headers": {
+            "accept": "*/*",
+            "accept-language": "en-GB,en;q=0.8",
+            "Referer": "https://sofi.gg/",
+            "Referrer-Policy": "strict-origin-when-cross-origin"
+        },
+        "body": null,
+        "method": "GET"
+    })).json()) as GlowAPIResponse).glows;
+    let currentGlows = glows.filter(glow => viewCodes.includes(glow.viewcode));
+    let idIndexMap = new Map(viewCodes.map((id, index) => [id, index]));
+    currentGlows.sort((a, b) => idIndexMap.get(a.viewcode)! - idIndexMap.get(b.viewcode)!);
+    const canvas = createCanvas(currentGlows.length * 150, 200);
+    const context = canvas.getContext("2d");
+    context.font = "26px Arial";
+    context.fillStyle = "#36393f";
+    context.fillRect(0, 150, currentGlows.length * 150 + 150, 200)
+    currentGlows.forEach((glow, index) => {
+        let fillStyle;
+        if (glow.super) {
+            const gradient = context.createLinearGradient(index * 150, 0, index * 150 + 150, 0);
+            gradient.addColorStop(0, processColor(glow.code));
+            gradient.addColorStop(1, processColor(glow.code2!));
+            fillStyle = gradient;
+        } else fillStyle = processColor(glow.code);
+        context.fillStyle = fillStyle;
+        context.fillRect(index * 150, 0, index * 150 + 150, 150);
+        context.fillStyle = "white";
+        context.fillText("#" + glow.viewcode, index * 150 + 20, 187.5);
+    });
+    const attachment = new AttachmentBuilder(canvas.toBuffer(), { name: "glowView.png" });
+    return [attachment, glows];
 }
 
 export function normalGlow(color: string, embedColor: number) {
@@ -88,9 +172,9 @@ export async function superGlow(owner: string, viewcode: string, embedColor: num
         },
         "body": null,
         "method": "GET"
-    })).json()) as any).glows.find((glow: any) => glow.viewcode == viewcode);
+    })).json()) as GlowAPIResponse).glows.find(glow => glow.viewcode == viewcode)!;
     const color1 = processColor(glow.code);
-    const color2 = processColor(glow.code2);
+    const color2 = processColor(glow.code2!);
     const canvas = createCanvas(150, 200);
     const context = canvas.getContext('2d');
     const image = await loadImage(`https://www.colorhexa.com/${color1.replace("#", "")}.png`);
